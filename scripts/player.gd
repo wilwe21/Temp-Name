ab4207eb872b193f5e709fc1af0eb34f7400724f
wilwe <wilwe036@gmail.com>
2024-06-11 22:42:47 +0200

    i had a stroke

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,31 +2,30 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var stamina = 50
 
 @onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		sprite.play('fly')
 	var direction = Input.get_axis("left", "right")
+	var direction2 = Input.get_axis("up", "down")
 	if direction:
-		velocity.x = direction * SPEED
-		if is_on_floor():
-			sprite.play('walk')
+		if !direction2:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = direction * SPEED/2
+		sprite.play('walk')
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if is_on_floor():
-			sprite.play('default')
-	if Input.is_action_just_pressed("up") and stamina != 0:
-		velocity.y = JUMP_VELOCITY
-		stamina -= 10
-	if is_on_floor():
-		stamina = 50
+	if direction2:
+		if !direction:
+			velocity.y = direction2 * SPEED
+		else:
+			velocity.y = direction2 * SPEED/2
+		sprite.play('walk')
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	if !direction && !direction2:
+		sprite.play('default')
 	if direction > 0:
 		sprite.flip_h = true
 	elif direction < 0:

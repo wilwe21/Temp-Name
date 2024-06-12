d11d8b1277e4e4c2b09d56f23f767977d72210b3
wilwe <wilwe036@gmail.com>
2024-06-12 13:37:50 +0200

    idk godot is fucked up where {} who can program in python syntax working game

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,33 +1,69 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 400.0
+var fly = false
+var alive = true
 
 @onready var sprite = $AnimatedSprite2D
+func set_fly():
+	if fly:
+		fly = false
+		set_collision_layer_value(2, false)
+		set_collision_mask_value(2, false)
+		set_collision_layer_value(1, true)
+		set_collision_mask_value(1, true)
+		emit_signal("fly_signal", false)
+	else:
+		fly = true
+		set_collision_layer_value(2, true)
+		set_collision_mask_value(2, true)
+		set_collision_layer_value(1, false)
+		set_collision_mask_value(1, false)
+		emit_signal("fly_signal", true)
 
 func _physics_process(delta):
-	var direction = Input.get_axis("left", "right")
-	var direction2 = Input.get_axis("up", "down")
-	if direction:
-		if !direction2:
-			velocity.x = direction * SPEED
+	if alive:
+		var direction = Input.get_axis("left", "right")
+		var direction2 = Input.get_axis("up", "down")
+		if Input.is_action_just_pressed("fly"):
+			set_fly()
+		if direction:
+			if !direction2:
+				velocity.x = direction * SPEED
+			else:
+				velocity.x = direction * SPEED/1.5
+			if !fly:
+				sprite.play('walk')
 		else:
-			velocity.x = direction * SPEED/2
-		sprite.play('walk')
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if direction2:
-		if !direction:
-			velocity.y = direction2 * SPEED
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		if direction2:
+			if !direction:
+				velocity.y = direction2 * SPEED
+			else:
+				velocity.y = direction2 * SPEED/1.5
+			if !fly:
+				sprite.play('walk')
 		else:
-			velocity.y = direction2 * SPEED/2
-		sprite.play('walk')
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+		if fly:
+			sprite.play('fly')
+		elif !direction && !direction2:
+			sprite.play('default')
+		if direction > 0:
+			sprite.flip_h = true
+		elif direction < 0:
+			sprite.flip_h = false
+		move_and_slide()
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-	if !direction && !direction2:
-		sprite.play('default')
-	if direction > 0:
-		sprite.flip_h = true
-	elif direction < 0:
-		sprite.flip_h = false
-	move_and_slide()
+		sprite.hide()
+
+func _on_area_2d_body_entered(body):
+	if !fly:
+		set_fly()
+
+
+func _on_area_2d_body_exited(body):
+	if fly:
+		set_fly()
+
